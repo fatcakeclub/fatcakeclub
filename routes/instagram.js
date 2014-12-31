825233a158f99229ea58dc38c3ebff9eb15f1e83
David Dunlop <ddunlop@gmail.com>
2014-12-31 16:09:28 -0500

    ability to fetch instagram access_token

--- routes/instagram.js
+++ routes/instagram.js
@@ -3,6 +3,7 @@
 var express = require('express'),
   debug = require('debug')('fatcakeclub:instagram'),
   instagramConfig = require('config').get('instagram'),
+  instagramBase = 'https://api.instagram.com/',
   // crypto = require('crypto'),
   url = require('url'),
   router = express.Router(),
@@ -10,11 +11,13 @@ var express = require('express'),
   request = require('request');
 
 function get(path, query, cb) {
-  var iurl = url.parse('https://api.instagram.com/v1' + path);
-    if(!cb) {
-      cb = query;
-      query = undefined;
-    }
+  var iurl = url.parse(instagramBase + path);
+
+  if(!cb) {
+    cb = query;
+    query = undefined;
+  }
+
   if(!iurl.query) {
     iurl.query = {};
   }
@@ -22,12 +25,11 @@ function get(path, query, cb) {
   iurl.query.access_token = instagramConfig.access_token;
   iurl = url.format(iurl);
   debug('iurl:', iurl);
-  console.log('console - iurl:', iurl);
-  request(url.format(iurl), function(err, respose, body) {
+  request(url.format(iurl), function(err, response, body) {
     if(err) {
       cb(err);
     }
-    else if(respose.statusCode !== 200) {
+    else if(response.statusCode !== 200) {
       cb({
         error: body
       });
@@ -38,9 +40,72 @@ function get(path, query, cb) {
   });
 }
 
+function post(path, data, cb) {
+  var iurl = url.parse(instagramBase + path);
+  request.post({
+    url:url.format(iurl),
+    form: data
+  }, function(err, response, body){
+    if(err) {
+      cb(err);
+    }
+    else if(response.statusCode !== 200) {
+      cb({
+        error: body
+      });
+    }
+    else {
+      cb(undefined, JSON.parse(body));
+    }
+  });
+}
+
+router.get('/access_token', function(req, res) {
+
+  var access_token_url = url.parse('https://api.instagram.com/oauth/authorize/'),
+    redirect_uri = url.format({
+      protocol: req.protocol,
+      host: req.get('host'),
+      pathname: '/instagram/access_token'
+    });
+
+  if(req.query.error) {
+    res
+      .status(401)
+      .render('instagram/access_token_error', {
+        error_reason: req.query.error_reason,
+        error_description: req.query.error_description
+    });
+    return;
+  }
+  if(req.query.code) {
+    post('/oauth/access_token', {
+      client_id: instagramConfig.client_id,
+      client_secret: instagramConfig.client_secret,
+      grant_type: 'authorization_code',
+      redirect_uri: redirect_uri,
+      code: req.query.code
+    }, function(err, body) {
+      res.render('instagram/access_token', {
+        access_token: body.access_token
+      });
+    });
+    return;
+  }
+
+
+  access_token_url.query = {
+    client_id: instagramConfig.client_id,
+    redirect_uri: redirect_uri,
+    response_type: 'code'
+  };
+
+  res.redirect(url.format(access_token_url));
+});
+
 /* GET instagram feed. */
 router.get('/', function(req, res) {
-  get('/tags/fatcakeclub/media/recent', {
+  get('/v1/tags/fatcakeclub/media/recent', {
     count: 24
   }, function(err, body) {
     res.send(body);
